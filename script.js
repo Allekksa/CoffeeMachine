57aec543d186fc383a44deb5a3875efa9f4a74ec
Allekksa <allekksa@gmail.com>
2021-05-26 17:41:13 +0300

    кофемашина умеет кликать по кнопкам в статусе выбирается название кофе и временно увеличивается баланс по клику

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
 "use strict";
-const log = console.log;
-log("скрипт подключен")
+// const log = console.log;
+// log("скрипт подключен");
+
+
+let items = document.querySelectorAll('.coffee-item');
+let balance = document.querySelector('.status-text');
+let progressBar = document.querySelector('.progress');
+let pay = document.querySelector('.money-status');
+for (let item of items){
+    item.addEventListener('click', buyCoffee);
+};
+
+function buyCoffee(name, price, element){
+    //    console.log([this.dataset.name, this.dataset.price, this])
+       progressBar.value = 10;
+
+    //    console.log(parseInt(pay.innerHTML))
+    //    if(parseInt(pay) === 0){
+    //      balance.innerHTML = "Недостаточно средств!"   
+    //    }
+       
+      balance.innerHTML = "Вы выбрали " + this.dataset.name;
+
+
+
+
+// balance.innerHTML = "Ваш " + this.dataset.name + " готовится!"
+
+};
+
+    let money = document.querySelector('.put-money');
+    // let pay = document.querySelector('.money-status');
+    // console.log([money, pay])
+        money.addEventListener('click', function(){
+        let countMoney =  parseInt(pay.innerHTML)
+        pay.innerHTML = ""
+        pay.innerHTML += Number(countMoney + 10)
+      
+    })
+
+
+//  while( Number(pay.innerHTML) < Number(this.dataset.price) ){
+//         balance.innerHTML = "Недостаточно средств!"   
+//        }
